aa27c3206e986b7f2353e78d4626f42bf4189bec
Sayantan <sayantann2002@gmail.com>
2022-09-04 11:19:06 +0530

    Deployment

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def feature_extraction(img_path,model):
 
 #Returns the indices of the recommended pictures
 def recommend(features,f_list):
-    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors = NearestNeighbors(n_neighbors=10, algorithm='brute', metric='euclidean')
     neighbors.fit(f_list)
 
     distances, indices = neighbors.kneighbors([features])
@@ -65,26 +65,33 @@ if uploaded_file is not None:
 
         # display the file
         display_image = Image.open(uploaded_file)
+        st.title('Query:')
         st.image(display_image)
 
         # feature extraction
         features = feature_extraction(os.path.join("upload",uploaded_file.name),model)
         
         indices = recommend(features,f_list)
-
+        st.title('Result:')
         # Display the similar images
-        col1,col2,col3,col4,col5 = st.columns(5)
+        col1,col2,col3,col4,col5,col6,col7,col8,col9,col10 = st.columns(10)
 
+        
         with col1:
-            st.image(filenames[indices[0][0]])
-        with col2:
-            st.image(filenames[indices[0][1]])
+            st.image(filenames[indices[0][0]], width=130)
+            st.image(filenames[indices[0][1]], width=130)
         with col3:
-            st.image(filenames[indices[0][2]])
-        with col4:
-            st.image(filenames[indices[0][3]])
+            st.image(filenames[indices[0][2]], width=130)
+            st.image(filenames[indices[0][3]], width=130)
         with col5:
-            st.image(filenames[indices[0][4]])
+            st.image(filenames[indices[0][4]], width=130)
+            st.image(filenames[indices[0][5]], width=130)
+        with col7:
+            st.image(filenames[indices[0][6]], width=130)
+            st.image(filenames[indices[0][7]], width=130)
+        with col9:
+            st.image(filenames[indices[0][8]], width=130)
+            st.image(filenames[indices[0][9]], width=130)
     else:
         st.header("Please upload the file again")
 
